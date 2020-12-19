3abfa753d722307cf42c1b9377278035f859ffe0
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-12-19 22:28:21 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,5 +3,12 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
+    Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      req.params['client_id'] = 'TVVTJ45J53WCH42DLYJSFW5J3BT25HDQXCQ1KBCK0ZKISWSG'
+      req.params['client_secret'] = 'U4VI1GYY3FF5G0EKMAXJWAPXJKOAOD0RXL23TAPFLI3KZTVK'
+      req.params['v'] = '20160201'
+      req.params['near'] = params[:zipcode]
+      req.params['query'] = 'coffee shop'
   end
+  render 'search'
 end
